8bb466f5d4a56c91c23814cae7dde8264ce1e49d
Kauam Tripolone <kauamts@gmail.com>
2020-11-27 03:30:44 -0300

    Starting the MainMenu
    Created the Scene, modified the camera to 2D and deleted the light

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    private AudioSource myAudio;
+    private string curScene;
+    private string lastScene;
+
+    public static bool pleaseDontStopTheMusic = true;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        myAudio = this.GetComponent<AudioSource>();
+
+        pleaseDontStopTheMusic = true;
+
+        if (!pleaseDontStopTheMusic)
+        {
+            myAudio.Stop();
+        }
+        else
+        {
+            myAudio.Play();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        curScene = SceneManager.GetActiveScene().name;
+
+        if (lastScene != curScene && curScene == "05_Parabens")
+        {
+            myAudio.Stop();
+        }
+
+        if (lastScene != curScene && curScene == "01_MainMenu")
+        {
+            AudioListener.volume = 1.0f;
+            AudioListener.pause = false;
+            myAudio.Play();
+        }
+
+        if (!pleaseDontStopTheMusic && myAudio.isPlaying == true)
+        {
+            myAudio.Pause();
+        }
+        else if (pleaseDontStopTheMusic && myAudio.isPlaying == false)
+        {
+            myAudio.UnPause();
+        }
+
+        lastScene = curScene;
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void ChangeScene(Scene myScene)
+    {
+        SceneManager.LoadScene(myScene.name, LoadSceneMode.Single);
     }
 }
