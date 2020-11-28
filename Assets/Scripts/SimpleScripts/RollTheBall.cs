e5f97f248c5543ddfbb955c18118151313ffb50a
Kauam Tripolone <kauamts@gmail.com>
2020-11-28 03:30:15 -0300

    MainMenu Finished and Level Created
    Finished the starting screen and started to create the level tiles and player character

---
+++ Assets/Scripts/SimpleScripts/RollTheBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollTheBall : MonoBehaviour
+{
+    public float xFloat;
+    public float yFloat;
+    public float zFloat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Rotate(xFloat, yFloat, zFloat);
+    }
+}
