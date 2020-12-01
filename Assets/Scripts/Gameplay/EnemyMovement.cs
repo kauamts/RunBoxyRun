b1e5342cd51d2f4a8dc79d5ace9cc22e5a3cce39
Kauam Tripolone <kauamts@gmail.com>
2020-11-30 21:05:47 -0300

    Enemy Spawning and Randomization Implemented
    Enemy spawning, speed and quantity were implemented and adjusted.

---
+++ Assets/Scripts/Gameplay/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float runningSpeed;
+
+    void Start()
+    {
+        runningSpeed = LevelController.runningSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if(this.transform.position.z > -30.00f)
+        {
+            this.transform.Translate(0f, 0f, runningSpeed*Time.deltaTime);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        runningSpeed = LevelController.runningSpeed;
+    }
+}
