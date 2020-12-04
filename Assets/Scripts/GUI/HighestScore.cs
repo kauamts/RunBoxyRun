1b76bcf7b3c9aea7d01c56e4d3b7bbad884e3d2b
Kauam Tripolone <kauamts@gmail.com>
2020-12-04 06:39:15 -0300

    Score and Highscore Implemented
    Both the GUI and Scripts for the Score and Highscore in the game were implemented.

---
+++ Assets/Scripts/GUI/HighestScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighestScore : MonoBehaviour
+{
+    private Text highestScoreText;
+
+    //Get the child component Text from the GameObject
+    void Start()
+    {
+        highestScoreText = GetComponent<Text>();
+    }
+
+    // Update the HighScore and send it to the Screen GUI Text
+    void Update()
+    {
+        if(GameManager.highestScore > 0)
+            highestScoreText.text = "Highscore: " + GameManager.highestScore.ToString("0");
+    }
+}
